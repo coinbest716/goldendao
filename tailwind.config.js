10098e1802bc4069cc39a12c2f3697f374208aa5
jupiter0913 <jupiter0913@protonmail.com>
2022-02-28 00:21:52 -0500

    updated the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      white: '#ffffff',
+      darkest_gold: '#895f1f',
+      dark_gold: '#b08000',
+      medium_gold: '#c6a000',
+      lightest_gold: '#f6d42e',
+      lightest_gray: '#efefef',
+      medium_gray: '#999999',
+    },
   },
   plugins: [],
 }
