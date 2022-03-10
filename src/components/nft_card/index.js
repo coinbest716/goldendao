b5059fc6054808fcb66638e318613cd68ce7273d
jupiter0913 <jupiter0913@protonmail.com>
2022-03-10 18:58:43 +0100

    fix video issue

--- src/components/nft_card/index.js
+++ src/components/nft_card/index.js
@@ -4,16 +4,18 @@ import { useRouter } from 'next/router'
 
 export default function NFTCard(props) {
   const router = useRouter()
-  function onClickCard(e) {
+  function onClickCard() {
     router.push('/nft')
-    e.preventDefault()
+  }
+  const stopPropagation = e => {
+    e.stopPropagation()
   }
   return (
     <div
       className={`relative bg-gradient-to-t from-darkest_gold to-medium_gold inline-block p-[20px] rounded-[16px] ${props.className}`}
       onClick={e => onClickCard(e)}
     >
-      <video width="690" autoPlay muted loop>
+      <video width="690" className="video-tag" autoPlay muted loop onClick={e => stopPropagation(e)}>
         <source src="gd-token.mp4" type="video/mp4" />
         <source src="gd-token.mp4" type="video/ogg" />
         Your browser does not support HTML video.
@@ -24,7 +26,7 @@ export default function NFTCard(props) {
         {/* <p className="text-[15pt]">1 ETH (2,624.28$)</p> */}
         <p className="text-[15pt]">1 ETH</p>
       </div>
-      <div className="absolute left-0 top-0 bottom-0 right-0" onClick={e => e.preventDefault()}></div>
+      <div className="absolute left-0 top-0 bottom-0 right-0 z-10"></div>
     </div>
   )
 }
