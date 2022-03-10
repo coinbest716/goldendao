9a2cd9a80665bf6484cffaaed9196a8bfc85550e
jupiter0913 <jupiter0913@protonmail.com>
2022-03-10 16:53:05 -0500

    added metamask even it's not installed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,6 +6,7 @@ import WalletConnectProvider from '@walletconnect/web3-provider'
 import { providers } from 'ethers'
 import WalletLink from 'walletlink'
 import Web3Modal from 'web3modal'
+import { providers as web3Providers } from 'web3modal'
 import { useSelector, useDispatch } from 'react-redux'
 import { setWeb3Provider, setAddress, resetAddress } from '@src/redux/actions/web3Actions'
 
@@ -54,6 +55,21 @@ const providerOptions = {
 
 let web3Modal
 if (typeof window !== 'undefined') {
+  if (!window.ethereum) {
+    providerOptions['custom-metamask'] = {
+      display: {
+        logo: web3Providers.METAMASK.logo,
+        name: 'Install MetaMask',
+        description: 'Connect using browser wallet',
+      },
+      package: {},
+      connector: async () => {
+        window.open('https://metamask.io')
+        throw new Error('MetaMask not installed')
+      },
+    }
+  }
+
   web3Modal = new Web3Modal({
     network: 'mainnet', // optional
     cacheProvider: true,
@@ -95,20 +111,24 @@ const Header = props => {
   }, [])
 
   const connect = useCallback(async function () {
-    // This is the initial `provider` that is returned when
-    // using web3Modal to connect. Can be MetaMask or WalletConnect.
-    const provider = await web3Modal.connect()
+    try {
+      // This is the initial `provider` that is returned when
+      // using web3Modal to connect. Can be MetaMask or WalletConnect.
+      const provider = await web3Modal.connect()
 
-    // We plug the initial `provider` into ethers.js and get back
-    // a Web3Provider. This will add on methods from ethers.js and
-    // event listeners such as `.on()` will be different.
-    const web3Provider = new providers.Web3Provider(provider)
+      // We plug the initial `provider` into ethers.js and get back
+      // a Web3Provider. This will add on methods from ethers.js and
+      // event listeners such as `.on()` will be different.
+      const web3Provider = new providers.Web3Provider(provider)
 
-    const signer = web3Provider.getSigner()
-    const address = await signer.getAddress()
+      const signer = web3Provider.getSigner()
+      const address = await signer.getAddress()
 
-    const network = await web3Provider.getNetwork()
-    dispatch(setWeb3Provider({ provider, web3Provider, address, chainId: network.chainId }))
+      const network = await web3Provider.getNetwork()
+      dispatch(setWeb3Provider({ provider, web3Provider, address, chainId: network.chainId }))
+    } catch (e) {
+      console.log('Error on Connection wallet')
+    }
   }, [])
 
   const disconnect = useCallback(
