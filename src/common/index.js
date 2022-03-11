c9a56302bb4782f6c8542d3efe5247aec0279f08
jupiter0913 <jupiter0913@protonmail.com>
2022-03-11 11:39:54 -0500

    updated the mailchip posturl

--- src/common/index.js
+++ src/common/index.js
@@ -1,1 +1,1 @@
-export const postUrl = `https://helloabdul.us20.list-manage.com/subscribe/post?u=lemonpepperbatteryfountaiN73!&id=golden_dao`
+export const postUrl = `https://xyz.us14.list-manage.com/subscribe/post?u=8e5f88856a97fcc0cba93419c&amp;id=2c231de5b1`
