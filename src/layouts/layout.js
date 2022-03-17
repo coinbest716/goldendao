d0842d9757021df9f885ca632f82b29bb57738f7
jupiter0913 <jupiter0913@protonmail.com>
2022-03-17 14:57:32 -0400

    added metadata

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -7,6 +7,13 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>Golden Dao</title>
+        <meta name="description" content="GoldenDao" />
+        <meta property="og:title" content="GoldenDao" />
+        <meta property="og:description" content="Also want these pretty website previews?" />
+        <meta property="og:url" content="http://richpreview.com/" />
+        <meta property="og:image" content="logo" />
+        <meta name="description" content="GoldenDao" />
+        <meta name="keywords" content="GoldenDao" />
       </Head>
       <div className="page-background">
         <Header />
